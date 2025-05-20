b7381f713f41ec6fdb77583d1768027338379772
MatheusMoselli <mat.moselli@hotmail.com>
2025-05-19 22:07:27 -0300

    feat: 70% accuracy

--- random_tree/train_random_forest.py
+++ random_tree/train_random_forest.py
@@ -4,15 +4,18 @@ from sklearn.metrics import classification_report
 
 train_data = pd.read_csv("datasets/training/filtered.csv")
 X_train = train_data.drop(columns=["class"])
-Y_train = train_data["class"]
-
+Y_train = (train_data["class"] == 0).astype(int)
 
 test_data = pd.read_csv("datasets/testing/filtered.csv")
 X_test = test_data.drop(columns=["class"])
-Y_test = test_data["class"]
+Y_test = (test_data["class"] == 0).astype(int)
 
 # Create and train model
-model = RandomForestClassifier(random_state=42, n_jobs=-2)
+model = RandomForestClassifier(
+    random_state=42,
+    n_jobs=-2
+)
+
 model.fit(X_train, Y_train)
 
 # Test models
