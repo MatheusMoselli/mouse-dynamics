8f602d00cc898f1eb0b82c6b01b205272bc8c10b
MatheusMoselli <mat.moselli@hotmail.com>
2026-05-17 18:14:26 -0300

    fix: code

--- src/utils/parquet_conversor.py
+++ src/utils/parquet_conversor.py
@@ -6,17 +6,14 @@ from src.dto.user_data_dto import UserDataDto
 def prepare_extraction_data_from_parquet(window_size: int) -> ExtractionData:
     split_path = Path("../datasets/split")
     extraction_data = ExtractionData()
-    
-    for user in split_path.iterdir():
-        user_id = user.stem.replace("user","")
+
+    for user in sorted(split_path.glob("user*")):
+        user_id = user.stem.replace("user", "")
         user_dto = UserDataDto(user_id)
-        
-        training_parquet = read_parquet(user / "training" / f"{window_size}" / "_merged.parquet")
-        user_dto.training_sessions = { "_merged": training_parquet }
-        
-        testing_parquet = read_parquet(user / "testing" / f"{window_size}" / "_merged.parquet")
-        user_dto.testing_sessions = { "_merged": testing_parquet }
-        
+
+        user_dto.training_sessions = {"_merged": read_parquet(user / str(window_size) / "training" / "_merged.parquet")}
+        user_dto.testing_sessions  = {"_merged": read_parquet(user / str(window_size) / "testing"  / "_merged.parquet")}
+
         extraction_data.add_user(user_dto)
-        
+
     return extraction_data
